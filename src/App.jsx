9a33fb0ec718cc556e81748cdcfc452b04809391
wonjongin <wonggamggik@gmail.com>
2025-01-09 15:32:10 +0900

    기본 설정 완료
    - prettier와 eslint 재설정
    
    - 헤더 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./assets/App.css";
 import HomePage from "./pages/HomePage";
-import FirstDraftPage from "./pages/FirstDraftPage";
-import SecondDraftPage from "./pages/SecondDraftPage";
-import ThirdDraftPage from "./pages/ThirdDraftPage";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="/first" element={<FirstDraftPage />}></Route>
-      <Route path="/second" element={<SecondDraftPage />}></Route>
-      <Route path="/third" element={<ThirdDraftPage />}></Route>
-    </Routes>
+    <BrowserRouter>
+      <div className="app">
+        <Header></Header>
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Heaer() {
+  return (
+    <div>
+      <div>
+        <a>
+          <img></img>
+        </a>
+      </div>
+    </div>
+  );
+}
